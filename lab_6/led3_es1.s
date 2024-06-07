99a24d74640bf4fbebc080a115bba9a07544ac66
mattia-gallinaro <gallinaromattia6@gmail.com>
2024-06-07 15:50:16 +0200

    es 1
    aggiunto qualche controllo

---
+++ lab_6/led3_es1.s
@@ -0,0 +1,38 @@
+.bss
+
+@ subroutine a disposizione
+@ set_gpio : inizializza la GPIO
+@ read_slides : leggere il valore impostato nei 4 switch e lo restituisce nei 4 bit meno significativi in R0
+@ write_led : visualizza nei led i 4 bit meno significativi di R0
+@ read_buttons : legge lo stato dei 2 pulsani e lo restituisce nei 2 bit meno significativi di R0.
+@             	ogni bit corrisponde allo stato di un pulsante.
+
+
+.text
+
+.global main
+main:
+    @ Salva i registri modificati
+    push {r0,r1,lr}
+    
+    @ inizializza la GPIO
+    bl set_gpio
+    
+    mov r0, #0
+    
+loop:   
+        bl read_buttons
+        cmp r0, #1
+        bleq read_slides
+        beq skip
+        cmp r0, #2
+        bleq read_slides
+        mvneq r0, r0
+        bne loop @cosi se l'utente non preme nulla allora skippa direttamente alla lettura successiva
+skip:   bl write_led
+        b loop
+
+    @ Ripristina registri modificati
+    pop {r0,r1,lr}
+    @ Termina e ritorna dalla funzione main
+    mov pc, lr
